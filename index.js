adfd914f46f61b4a03806cc6738ca760606f2e79
Georgi Petrushev <figoto660@gmail.com>
2019-10-15 01:19:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 var nameArr = [];
 
+// let  takeANumber = (nameArr, name) =>{
+// nameArr.push(`${name}`);
+// return(`Welcome, ${name}. You are number ${nameArr.length} in line.`);
+// }
 
-function takeANumber(nameArr, name){
- nameArr.push(`${name}`);
- return(`Welcome, ${name}. You are number ${nameArr.length} in line.`);
-}
 
+let nowServing =(nameArr) =>{
 
-function nowServing(nameArr){
-   
+ 
+
   if(nameArr.length === 0){
     return "There is nobody waiting to be served!";
   }
   
   else
+
   return (`Currently serving ${nameArr.shift()}.`);
   
   
 }
 
 var line = [];
-function currentLine(nameArr) {
+let currentLine = (nameArr) => {
   let i = 0;
   while (i < nameArr.length) {
     line.push(` `+[i+1]+`. `  + nameArr[i])
@@ -28,6 +30,12 @@ function currentLine(nameArr) {
   }
   if (nameArr.length === 0) {
     return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
+  } else{
+  return(`The line is currently:${line}`);
 }
+
+  return (`Currently serving ${nameArr[0]}.`);
+  
+  
+}
+
